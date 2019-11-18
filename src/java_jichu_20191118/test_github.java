26143c1af8b213058e5fe710987071bc26c20803
ydl1997 <1002426524@qq.com>
2019-11-18 10:15:18 +0800

    本人学习java的所有轨迹-version：0.0.1

---
+++ src/java_jichu_20191118/test_github.java
@@ -0,0 +1,7 @@
+package java_jichu_20191118;
+//测试idea是否正常运行以及项目是否可以同步到github
+public class test_github {
+    public static void main(String[] args) {//快捷输入psvm
+        System.out.println("hello idea and github");//快捷输入sout
+    }
+}
