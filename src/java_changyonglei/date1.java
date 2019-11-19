d2a603038f16dd862aae5f7d536dd5bd03f2644b
ydl1997 <1002426524@qq.com>
2019-11-19 18:53:17 +0800

    获取时间戳，为long类型

---
+++ src/java_changyonglei/date1.java
@@ -0,0 +1,11 @@
+package java_changyonglei;
+//date类util包下的
+import java.util.Date;
+
+public class date1 {
+    public static void main(String[] args) {
+        Date a =new Date();
+        System.out.println(a.getTime());
+
+    }
+}
