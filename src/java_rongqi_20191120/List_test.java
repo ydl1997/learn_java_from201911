b4ab1bb46012007e12c3e69ef19781811eb6d595
ydl1997 <1002426524@qq.com>
2019-11-20 15:28:46 +0800

    创建list的添加元素类

---
+++ src/java_rongqi_20191120/List_test.java
@@ -0,0 +1,27 @@
+package java_rongqi_20191120;
+
+public class List_test {
+    private String name;
+    private  String sex;
+
+    public List_test(String name, String sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
